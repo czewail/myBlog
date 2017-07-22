6553dc605873b5b7d3efc7597cbce81a467a72c1
陈泽韦 <549226266@qq.com>
2017-07-23 01:26:45 +0800

    add

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -141,6 +141,20 @@
 <script type="text/javascript" src="{{asset('front/js/app.js')}}"></script>
 @yield('js')
 <script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
